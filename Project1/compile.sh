db1c960b6f9b0dffd0fe21bbd922d3e1e4d61c92
Shawn Berg <bergshawnm@gmail.com>
2019-04-16 12:27:06 -0500

    Changed output to csv

--- Project1/compile.sh
+++ Project1/compile.sh
@@ -8,8 +8,8 @@ for trials in 1 10 50 100 500 1000 5000 10000 50000 1000000 500000 1000000 50000
 do
     for threads in 1 2 4 6 8
     do
-        #g++-8 -DNUMT=$t -o proj1 main.cpp -lm -fopenmp
-        g++ -DNUMT=$threads -DNUMTRIALS=$trials -o proj1 main.cpp -lm -fopenmp
-        ./proj1 >> output.txt
+        g++-8 -DNUMT=$threads -DNUMTRIALS=$trials -o proj1 main.cpp -lm -fopenmp
+        #g++ -DNUMT=$threads -DNUMTRIALS=$trials -o proj1 main.cpp -lm -fopenmp
+        ./proj1 >> output.csv
     done
 done
