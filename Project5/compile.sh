a9999647286b41a2a711351ab441cebc76ccba9f
Shawn Berg <bergshawnm@gmail.com>
2019-05-21 22:58:29 +0000

    Working on GCP

--- Project5/compile.sh
+++ Project5/compile.sh
@@ -3,18 +3,18 @@
 machine=$(uname)
 alias macCompile='g++-9 -DNUM_ELEMENTS=$globalSize -DLOCAL_SIZE=$localSize -o proj5 main.cpp -Wno-write-strings -lm -framework OpenCL -fopenmp
 '
-alias linuxCompile='g++ -DNUM_ELEMENTS=$globalSize -DLOCAL_SIZE=$localSize -o proj5 main.cpp -Wno-write-strings -lm -lOpenCL -fopenmp'
+alias linuxCompile='g++ -DNUM_ELEMENTS=$globalSize -DLOCAL_SIZE=$localSize -o proj5 main.cpp -Wno-write-strings -Wno-format -lm -lOpenCL -fopenmp'
 
 rm output.csv
-echo " , 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 8388608" >> output.csv
+echo " , 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 8388608, 16777216, 33554432" >> output.csv
 for clFile in "arrMult.cl" "arrMultAdd.cl"
 do
     #printf "clFile: %s\n" "$clFile" >> output.csv
-    for localSize in 8 16 32 64 128 256 512
+    for localSize in 8 16 32 64 128 256 512 1024
     do
         #printf "$globalSize, " >> output.csv
         printf "%s," "$localSize" >> output.csv
-        for globalSize in 1024 4096 16384 65536 262144 1048576 4194304 8388608
+        for globalSize in 1024 4096 16384 65536 262144 1048576 4194304 8388608 16777216 33554432 67108864
         do
             #printf "Elements: %s, Local Size: %s\n" "$globalSize" "$localSize"
             if [ "$machine" = "Darwin" ]; then
