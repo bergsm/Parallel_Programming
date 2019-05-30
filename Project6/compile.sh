f025e7ef4963084ad579abb8465dd2a0667192dc
Shawn Berg <bergshawnm@gmail.com>
2019-05-30 12:17:48 -0500

    Modified compilation script

--- Project6/compile.sh
+++ Project6/compile.sh
@@ -1,19 +1,20 @@
 #~/bin/bash
 
+#TODO compile for CUDA
 machine=$(uname)
-alias macCompile='g++-8 -DNUMT=$threads -DNUMTRIALS=$trials -DLAST=$last -o proj6 main.cpp -lm -fopenmp'
-alias linuxCompile='g++ -DNUMT=$threads -DNUMTRIALS=$trials -DLAST=$last -o proj6 main.cpp -lm -fopenmp'
+alias macCompile='g++-8 -DBLOCKSIZE=$blocksize -DNUMTRIALS=$trials -DLAST=$last -o proj6 main.cpp -lm -fopenmp'
+alias linuxCompile='g++ -DBLOCKSIZE=$blocksize -DNUMTRIALS=$trials -DLAST=$last -o proj6 main.cpp -lm -fopenmp'
 last=0
 
 rm output.csv
 echo " , 1, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000" >> output.csv
 
-for threads in 1 2 4 6 8
+for blocksize in 16 32 64
 do
-    printf "$threads, " >> output.csv
-    for trials in 1 10 50 100 500 1000 5000 10000 50000 100000 500000 1000000 5000000
+    printf "$blocksize, " >> output.csv
+    for trials in 16000 32000 64000 128000 256000 512000
     do
-        if [ "$threads" -eq "8" ] && [ "$trials" -eq "5000000" ]; then
+        if [ "$blocksize" -eq "64" ] && [ "$trials" -eq "512000" ]; then
             last=1
         fi
         if [ "$machine" = "Darwin" ]; then
